522a00f6d4d36887b9a1cf0b336ec8a1922340f6
Segni <segnides@gmail.com>
2021-12-25 09:40:59 +0300

    added grading student solution

---
+++ grading_student.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    for i in range(len(grades)):
+        if grades[i]<38:
+            continue
+        if (grades[i]+1)%5==0:
+            grades[i]+=1
+            continue
+        if (grades[i]+2)%5==0:
+            grades[i]+=2
+    return grades
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
