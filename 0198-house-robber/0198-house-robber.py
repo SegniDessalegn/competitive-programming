4042fa98485de26c0d7161bcc81d9bec42932cd6
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2022-12-17 16:50:35 +0300

    Time: 67 ms (13.66%), Space: 14 MB (17.46%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+        def dp(i = 0):
+            if i >= len(nums):
+                return 0
+            if i in memo:
+                return memo[i]
+            memo[i] = max(nums[i] + dp(i + 2), dp(i + 1))
+            return memo[i]
+        
+        return dp()
+    
