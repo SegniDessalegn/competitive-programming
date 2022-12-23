74db098c3991f14c97da194fb7baab07e05f429b
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2022-12-23 09:46:57 +0300

    Time: 134 ms (15.89%), Space: 14.7 MB (39.23%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left].isalnum() and s[right].isalnum():
+                if s[left].lower() != s[right].lower():
+                    return False
+                left += 1
+                right -= 1
+            else:
+                if not s[left].isalnum():
+                    left += 1
+                else:
+                    right -= 1
+        return True
