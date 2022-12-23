3330af8195b9cc266b67b47804e32d444f13e71f
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2022-12-23 10:11:21 +0300

    Time: 168 ms (75.08%), Space: 14.6 MB (47.27%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return self.check(s[left:right]) or self.check(s[left + 1:right + 1])
+            left += 1
+            right -= 1
+        return True
+        
+    def check(self, s):
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
