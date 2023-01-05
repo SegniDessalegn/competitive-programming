b75953b63445bf671e2fdc84b17c1e90b466fb01
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-01-05 12:54:55 +0300

    Time: 3096 ms (37.45%), Space: 59.9 MB (70.29%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        # sort the points
+        points.sort()
+        
+        # we can track intersection of points using end of a range
+        end = points[0][1]
+        arrows = 1
+        for i in range(1, len(points)):
+            # if points[i][0] is greater than end, increament arrows and update the end
+            # now end is our new checking range
+            if points[i][0] > end:
+                arrows += 1
+                end = points[i][1]
+            else:
+                # if points[i][1] is less than end, update new to points[i][1]
+                end = min(end, points[i][1])
+        
+        return arrows
