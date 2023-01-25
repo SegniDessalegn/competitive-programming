208936096b47d66141ba6cbe25fa41784dae18ff
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-01-25 17:15:05 +0300

    Time: 2923 ms (54.15%), Space: 26.4 MB (39.33%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        m = len(box)
+        n = len(box[0])
+        for i in range(m):
+            curr = 0
+            j = 0
+            while j < n:
+                if box[i][j] == ".":
+                    box[i][j], box[i][curr] = box[i][curr], box[i][j]
+                    curr += 1
+                elif box[i][j] == "*":
+                    curr = j + 1
+                j += 1
+        
+        mat = [[None for i in range(m)] for j in range(n)]
+        for i in range(m):
+            for j in range(n):
+                mat[j][i] = box[-i - 1][j]
+        
+        return mat
