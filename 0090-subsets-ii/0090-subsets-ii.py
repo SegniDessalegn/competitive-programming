c23c5ae1ae9213adfffa2183acec6cd68d3be870
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-03-10 18:12:47 +0300

    Time: 41 ms (54.93%), Space: 14.2 MB (6.70%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -4,12 +4,15 @@ class Solution:
         def recur(nums, curr_ans = []):
             if not nums:
                 return
-            
-            for i in range(len(nums)):
+            i = 0
+            while i < len(nums):
                 curr_ans.append(nums[i])
                 solutions.add(tuple(curr_ans))
                 recur(nums[i + 1:], curr_ans)
                 curr_ans.pop()
+                i += 1
+                while i < len(nums) and nums[i] == nums[i - 1]:
+                    i += 1
         
         nums.sort()
         recur(nums)
