555702ac65b1578b503a9fa6493f03d17690a568
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-03-14 20:59:07 +0300

    Time: 80 ms (59.55%), Space: 14 MB (51.80%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -9,6 +9,7 @@ class Solution:
             elif total > target:
                 return
             
+            # prev is used for pruning, if we did combinations for a number, we do not have to do the same combinations for the same number
             prev = -1
             for j in range(i, len(candidates)):
                 if prev == candidates[j]:
@@ -18,6 +19,7 @@ class Solution:
                 curr_ans.pop()
                 prev = candidates[j]
         
+        # we have to sort the candidates because it is easier to prune, which also avoids duplicate combinations
         candidates.sort()
         recur()
         return solutions
