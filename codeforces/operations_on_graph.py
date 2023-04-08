e8d82d8a6699948d9ec12551537d7512b2a8b58f
Segni Dessalegn <segnides@gmail.com>
2023-04-08 22:54:54 +0300

    update operations on graph

--- codeforces/operations_on_graph.py
+++ codeforces/operations_on_graph.py
@@ -2,14 +2,11 @@
 vertexes = int(input())
 operations = int(input())
 
-graph = {i + 1:set() for i in range(vertexes)}
+graph = {i + 1:[] for i in range(vertexes)}
 for _ in range(operations):
     curr = tuple(map(int, input().split()))
     if curr[0] == 1:
-        graph[curr[1]].add(curr[2])
-        graph[curr[2]].add(curr[1])
+        graph[curr[1]].append(curr[2])
+        graph[curr[2]].append(curr[1])
     else:
-        if graph[curr[1]]:
-            print(*graph[curr[1]])
-        else:
-            print()
+        print(*graph[curr[1]])
