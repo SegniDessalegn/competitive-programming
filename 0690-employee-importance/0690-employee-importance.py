df48a84dcb97915afeea2eb0f8a34b4d1c61eeb4
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-04-10 14:21:05 +0300

    Time: 153 ms (47.65%), Space: 15.5 MB (50.43%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,28 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        graph = defaultdict(list)
+        for emp in employees:
+            graph[emp.id] = [emp.importance, emp.subordinates]
+        
+        queue = deque([graph[id]])
+        visited = set([id])
+        total_importance = graph[id][0]
+        while queue:
+            imp, neighbours = queue.pop()
+            for n in neighbours:
+                if n not in visited:
+                    total_importance += graph[n][0]
+                    queue.append(graph[n])
+                    visited.add(n)
+        
+        return total_importance
+    
