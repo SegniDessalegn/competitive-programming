172310ef1da4237adcbb23a7f732723c85518a3a
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-04-27 18:17:28 +0300

    Time: 651 ms (48.26%), Space: 17.9 MB (6.43%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -2,7 +2,6 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         queue = deque([(0, 0, 1)])
         visited = set([(0, 0)])
-        ans = -1
         rows = len(grid)
         cols = len(grid[0])
         directions = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
@@ -11,10 +10,7 @@ class Solution:
             if grid[x][y] == 1:
                 continue
             elif (x, y) == (rows - 1, cols - 1):
-                if ans == -1:
-                    ans = dist
-                else:
-                    ans = min(ans, dist)
+                return dist
             for dx, dy in directions:
                 r, c = x + dx, y + dy
                 if 0 <= r < rows and 0 <= c < cols:
@@ -22,4 +18,4 @@ class Solution:
                         queue.append((r, c, dist + 1))
                         visited.add((r, c))
         
-        return ans
+        return -1
