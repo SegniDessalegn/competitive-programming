67272f356ae173e67d61f6c9355f339b42562fc8
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-04-27 18:13:05 +0300

    Time: 542 ms (84.96%), Space: 17.8 MB (5.72%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,27 +1,25 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         deadends = set(deadends)
-        queue = deque([("0000", 0)])
-        visited = set("0000")
         if "0000" in deadends:
             return -1
+        nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        queue = deque([("0000", 0)])
+        visited = set("0000")
         while queue:
             curr, turns = queue.popleft()
             if curr == target:
                 return turns
-            for i in range(len(curr)):
-                if curr[i] != "9":
-                    code = curr[:i] + str(int(curr[i]) + 1) + curr[i + 1:]
-                else:
-                    code = curr[:i] + "0" + curr[i + 1:]
-                if code not in visited and code not in deadends:
-                    queue.append((code, turns + 1))
-                    visited.add(code)
-                if curr[i] != "0":
-                    code = curr[:i] + str(int(curr[i]) - 1) + curr[i + 1:]
-                else:
-                    code = curr[:i] + "9" + curr[i + 1:]
-                if code not in visited and code not in deadends:
-                    queue.append((code, turns + 1))
-                    visited.add(code)
+            for i in range(4):
+                turn1 = curr[:i] + nums[(int(curr[i]) + 1) % 10] + curr[i + 1:]
+                if turn1 not in visited and turn1 not in deadends:
+                    queue.append((turn1, turns + 1))
+                    visited.add(turn1)
+                turn2_index = int(curr[i]) - 1
+                if turn2_index == -1:
+                    turn2_index = 9
+                turn2 = curr[:i] + nums[turn2_index] + curr[i + 1:]
+                if turn2 not in visited and turn2 not in deadends:
+                    queue.append((turn2, turns + 1))
+                    visited.add(turn2)
         return -1
