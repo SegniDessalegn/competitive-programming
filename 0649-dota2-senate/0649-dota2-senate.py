c10ba57557c66bc267e551541aac07098574210b
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-05-04 07:59:37 +0300

    Time: 66 ms (54.43%), Space: 16.6 MB (7.03%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,34 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        R_count = senate.count("R")
+        D_count = senate.count("D")
+        
+        senate = deque(senate)
+        turn = None
+        turn_count = 0
+        while True:
+            length = len(senate)
+            if length == 1:
+                break
+            
+            turn_not_changed = True
+            for i in range(length):
+                curr = senate.popleft()
+                if curr == turn:
+                    turn_count += 1
+                    senate.append(curr)
+                else:
+                    if turn == None:
+                        turn = curr
+                        senate.append(curr)
+                        turn_count = 1
+                    else:
+                        turn_count -= 1
+                        if turn_count == 0:
+                            turn = None
+                            turn_not_changed = False
+            
+            if turn_not_changed:
+                break
+        
+        return "Radiant" if senate[0] == "R" else "Dire"
