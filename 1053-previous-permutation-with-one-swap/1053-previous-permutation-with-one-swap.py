724a799fb7c6db4efa32dcbe9952ec8d24ab1dc4
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-05-31 07:09:11 +0300

    Time: 235 ms (57.09%), Space: 17.9 MB (14.56%) - LeetHub

--- 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
@@ -4,11 +4,21 @@ class Solution:
         # we have to traverse backwards to get largest permutation
         
         n = len(arr)
+        swap = None
+        swap_index = None
         for i in range(n - 2, -1, -1):
             if arr[i] > arr[i + 1]:
-                for j in range(n - 1, i, -1):
-                    if arr[j] < arr[i] and (j == i - 1 or arr[j] != arr[j - 1]):
-                        arr[i], arr[j] = arr[j], arr[i]
-                        return arr
+                swap = arr[i]
+                swap_index = i
+                break
+        
+        if swap:
+            swap2_index = None
+            for i in range(n - 1, swap_index, -1):
+                if arr[i] < arr[swap_index] and arr[i] != arr[i - 1]:
+                    swap2_index = i
+                    break
+            
+            arr[swap_index], arr[swap2_index] = arr[swap2_index], arr[swap_index]
         
         return arr
