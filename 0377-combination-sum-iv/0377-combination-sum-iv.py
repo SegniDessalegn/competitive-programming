e6fd85739429984c188f4ed01e5ef5f597413b22
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-06-01 19:13:11 +0300

    Time: 404 ms (5.09%), Space: 27.4 MB (6.63%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        
+        n = len(nums)
+        @cache
+        def back_track(curr, i):            
+            if curr == target:
+                return 1
+            elif curr > target:
+                return 0
+            
+            curr_ans = 0
+            for j in range(n):
+                curr_ans += back_track(curr + nums[j], j)
+            
+            return curr_ans
+        
+        return back_track(0, 0)
