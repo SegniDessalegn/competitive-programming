7b388cd14b796eaaa016906fed7941aecec85d0e
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-06-08 20:17:01 +0300

    Time: 131 ms (83.76%), Space: 17.4 MB (62.34%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,24 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        
-        def recur(i):
-            if i == n - 1:
-                return 0
-            elif i >=n or nums[i] == 0:
-                return float("inf")
-            
-            if i in dp:
-                return dp[i]
-            
-            min_steps = float("inf")
-            for step in range(1, nums[i] + 1):
-                min_steps = min(min_steps, 1 + recur(i + step))
-            
-            dp[i] = min_steps
-            
-            return dp[i]
-        
-        
+        steps = 0
+        start = far = 0
         n = len(nums)
-        dp = {}
-        return recur(0)
+        for i in range(n):
+            if i + nums[i] > far:
+                far = i + nums[i]
+            if i == start and i != n - 1:
+                steps += 1
+                start = far
+        
+        return steps
