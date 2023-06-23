2ed3980b1d114d5ef4391d3664a1d5aa50c19f7d
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-06-23 20:19:06 +0300

    Time: 805 ms (11.08%), Space: 18.1 MB (5.54%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -2,24 +2,16 @@ class Solution:
     def isPossible(self, nums: List[int]) -> bool:
         heaps = defaultdict(list)
         for n in nums:
-            found = False
             popped = []
-            while heaps[n - 1]:
-                popped.append(heapq.heappop(heaps[n - 1]))
-                if -popped[-1][1][0] + 1== n:
-                    found = True
-                    break
+            if heaps[n - 1]:
+                popped = heapq.heappop(heaps[n - 1])
             
-            if not found:
-                while popped:
-                    heapq.heappush(heaps[n], popped.pop())
+            if not popped:
                 heapq.heappush(heaps[n], (1, [-n]))
             else:
-                length, heap = popped.pop()
+                length, heap = popped
                 heapq.heappush(heap, -n)
                 heapq.heappush(heaps[n], (length + 1, heap))
-                while popped:
-                    heapq.heappush(heaps[n], popped.pop())
         
         for heap in heaps:
             for arr in heaps[heap]:
