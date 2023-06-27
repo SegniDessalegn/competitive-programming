22b873d67ceade508770ffed63fb05e8f61aaeef
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-06-27 21:15:20 +0300

    Time: 5636 ms (5.10%), Space: 20.1 MB (12.10%) - LeetHub

---
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maximalPathQuality(self, values: List[int], edges: List[List[int]], maxTime: int) -> int:
+        graph = defaultdict(list)
+        time = {}
+        for edge in edges:
+            a, b, t = edge
+            graph[a].append(b)
+            graph[b].append(a)
+            time[(a, b)] = t
+            time[(b, a)] = t
+        
+        
+        def recur(node, currTime, added):
+            nonlocal ans
+            if currTime <= maxTime:
+                if node == 0:
+                    currSum = 0
+                    for n in added:
+                        currSum += values[n]
+                    ans = max(ans, currSum)
+            else:
+                return
+            
+            for n in graph[node]:
+                recur(n, currTime + time[(node, n)], added.union(set([n])))
+        
+        ans = 0
+        recur(0, 0, set([0]))
+        return ans
