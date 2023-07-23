ed5d405abcc5da3fb55e2b3fc8cef983c26ea5aa
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-07-23 21:18:23 +0300

    Time: 285 ms (56.42%), Space: 25.6 MB (29.78%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -1,6 +1,5 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        
         directions = [(-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2)]
         
         @cache
@@ -8,14 +7,20 @@ class Solution:
             if not (0 <= i < n and 0 <= j < n):
                 return 0
             
-            if move == k:
+            if move == 0:
                 return 1
             
-            p = 0
+            count = 0
             for dx, dy in directions:
                 r, c = i + dx, j + dy
-                p += recur(move + 1, r, c)
+                if (0 <= r < n and 0 <= c < n):
+                    count += 1
             
-            return p / 8
+            curr_p = 0
+            for dx, dy in directions:
+                r, c = i + dx, j + dy
+                curr_p += (count * recur(move - 1, r, c))
+            
+            return 0 if count == 0 else curr_p / (count * 8)
         
-        return recur(0, row, column)
+        return recur(k, row, column)
