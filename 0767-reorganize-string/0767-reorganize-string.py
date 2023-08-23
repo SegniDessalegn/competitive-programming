6ccade88147a1c37cbe52ad8fe37f434e1e60d2d
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-08-23 07:36:06 +0300

    Time: 43 ms (71.50%), Space: 16.3 MB (46.15%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        s = list(s)
+        N = len(s)
+        count = Counter(s)
+        
+        heap = []
+        for char in count:
+            heappush(heap, (-count[char], char))
+        
+        ans = []
+        while heap:
+            curr = heapq.heappop(heap)
+            if ans and curr[1] == ans[-1]:
+                if not heap:
+                    return ""
+                curr2 = heapq.heappop(heap)
+                ans.append(curr2[1])
+                if -curr2[0] > 1:
+                    heapq.heappush(heap, (curr2[0] + 1, curr2[1]))
+            ans.append(curr[1])
+            if -curr[0] > 1:
+                heapq.heappush(heap, (curr[0] + 1, curr[1]))
+            
+        return "".join(ans)
