17d59e9dbe9da567eab0de04743e5c1b9bb417ab
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-08-31 13:50:51 +0300

    Time: 135 ms (70.71%), Space: 18.8 MB (15.00%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minTaps(self, n: int, ranges: List[int]) -> int:
+        points = []
+        for i in range(n + 1):
+            points.append((max(0, i - ranges[i]), min(n, i + ranges[i])))
+        
+        points.sort(key = lambda X:(X[0], -X[1]))
+        if not points:
+            return -1
+        
+        ans = 1
+        reach = points[0][1]
+        next_reach = 0
+        for i in range(len(points)):
+            if points[i][0] > reach:
+                # update reach
+                if next_reach > reach:
+                    reach = next_reach
+                    ans += 1
+                else:
+                    return -1
+            next_reach = max(next_reach, points[i][1])
+        
+        if reach != n:
+            ans += 1
+        
+        return ans if next_reach == n else -1
