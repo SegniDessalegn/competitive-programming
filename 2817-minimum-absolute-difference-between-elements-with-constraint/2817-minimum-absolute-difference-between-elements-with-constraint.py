91eb837642892c6759f23e4cd0c0f472050c6939
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-09-01 09:48:52 +0300

    Time: 4017 ms (9.15%), Space: 31.8 MB (70.25%) - LeetHub

--- 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
@@ -2,13 +2,20 @@ from sortedcontainers import SortedList
 
 class Solution:
     def minAbsoluteDifference(self, nums: List[int], x: int) -> int:
-        s = SortedList()
         N = len(nums)
+        s = SortedList()
+        
         ans = float("inf")
-        for i in range(x, N):
-            s.add(nums[i - x])
-            idx = bisect_left(s, nums[i])
-            
-            ans = min(ans, abs(nums[i] - s[min(len(s) - 1, idx)]), abs(nums[i] - s[idx - 1]), abs(nums[i] - s[min(len(s) - 1, idx + 1)]))
-
+        
+        for i in range(N):
+            if i >= x:
+                s.add(nums[i - x])
+                min_idx = bisect_left(s, nums[i])
+                if min_idx > 0:
+                    ans = min(ans, abs(nums[i] - s[min_idx - 1]))
+                if min_idx < len(s):
+                    ans = min(ans, abs(nums[i] - s[min_idx]))
+                if min_idx < len(s) - 1:
+                    ans = min(ans, abs(nums[i] - s[min_idx + 1]))
+        
         return ans
