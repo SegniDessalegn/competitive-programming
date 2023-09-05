5d2bd2c1172871fa99ca35ae1ed819cd20cb7556
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-09-05 21:58:11 +0300

    Time: 429 ms (92.99%), Space: 17.3 MB (5.09%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,20 +1,9 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        N = len(nums1)
-        freq = Counter(nums4)
-        
-        @cache
-        def two_sum(target):
-            curr_count = 0
-            for n in nums3:
-                if target - n in freq:
-                    curr_count += freq[target - n]
-            
-            return curr_count
-        
+        freq = Counter([a + b for a in nums3 for b in nums4])
         count = 0
-        for i in range(N):
-            for j in range(N):
-                count += two_sum(-nums1[i] - nums2[j])
+        for a in nums1:
+            for b in nums2:
+                count += freq[-a - b]
         
         return count
