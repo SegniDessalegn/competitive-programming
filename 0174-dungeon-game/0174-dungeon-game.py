16985002d72fbd58371ea24e3c0688a735b7987d
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-09-18 18:34:36 +0300

    Time: 81 ms (28.95%), Space: 17.4 MB (89.24%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -1,27 +1,32 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
-        m = len(dungeon)
-        n = len(dungeon[0])
-        dp = {(i, j): 0 for j in range(n) for i in range(m)}
-        dp[(m - 1, n - 1)] = 1 if dungeon[-1][-1] >= 0 else -dungeon[-1][-1] + 1
-        for j in range(n - 2, -1, -1):
-            prev = dp[(m - 1, j + 1)]
-            needed = prev - dungeon[m - 1][j]
-            needed = max(1, needed)
-            dp[(m - 1, j)] = needed
+        M = len(dungeon)
+        N = len(dungeon[0])
         
-        for i in range(m - 2, -1, -1):
-            prev = dp[(i + 1, n - 1)]
-            needed = prev - dungeon[i][n - 1]
-            needed = max(1, needed)
-            dp[(i, n - 1)] = needed
+        dp = [[0] * N for _ in range(M)]
+        dp[M - 1][N - 1] = 1 if dungeon[M - 1][N - 1] > 0 else -dungeon[M - 1][N - 1] + 1
+        for j in range(N - 2, -1, -1):
+            prev = dp[M - 1][j + 1]
+            if dungeon[M - 1][j] > 0:
+                prev -= dungeon[M - 1][j]
+                dp[M - 1][j] = max(1, prev)
+            else:
+                dp[M - 1][j] = -dungeon[M - 1][j] + prev
         
-        for i in range(m - 2, -1, -1):
-            for j in range(n - 2, -1, -1):
-                bottom = dp[(i + 1, j)]
-                right = dp[(i, j + 1)]
+        for i in range(M - 2, -1, -1):
+            prev = dp[i + 1][N - 1]
+            if dungeon[i][N - 1] > 0:
+                prev -= dungeon[i][N - 1]
+                dp[i][N - 1] = max(1, prev)
+            else:
+                dp[i][N - 1] = -dungeon[i][N - 1] + prev
+        
+        for i in range(M - 2, -1, -1):
+            for j in range(N - 2, -1, -1):
+                bottom = dp[i + 1][j]
+                right = dp[i][j + 1]
                 needed = min(bottom, right) - dungeon[i][j]
                 needed = max(1, needed)
-                dp[(i, j)] = needed
+                dp[i][j] = needed
         
-        return dp[(0, 0)]
+        return dp[0][0]
