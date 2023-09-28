295781d3e5e9141f5334b030b19568a03a9c5301
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-09-28 10:32:20 +0300

    Time: 1068 ms (31.91%), Space: 43.9 MB (5.08%) - LeetHub

---
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self):
+        self.nodes = {}
+        self.end = 0
+
+class Trie:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        
+        for char in word:
+            if char not in curr.nodes:
+                curr.nodes[char] = Node()
+            curr = curr.nodes[char]
+        
+        curr.end += 1
+
+
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        '''
+        - insert the words to a trie
+        - do a dfs on the trie
+        '''
+        
+        T = Trie()
+        for word in words:
+            T.insert(word)
+        
+        N = len(s)
+        index = defaultdict(list)
+        for i in range(N):
+            index[s[i]].append(i)
+        
+        def count(root, word, i):
+            nonlocal ans
+            if i == len(word):
+                return
+            
+            for char in root.nodes:
+                idx = bisect_left(index[char], i)
+                if idx < len(index[char]):
+                    ans += root.nodes[char].end
+                    idx = index[char][idx]
+                    count(root.nodes[char], word, idx + 1)
+        
+        ans = 0
+        count(T.root, s, 0)
+        
+        return ans
+    
