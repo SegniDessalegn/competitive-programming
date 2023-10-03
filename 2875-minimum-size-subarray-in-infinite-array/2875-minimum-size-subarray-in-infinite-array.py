82c899ae0449c888bed2d5990ced8567a9566130
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-10-03 07:18:40 +0300

    Time: 309 ms (99.83%), Space: 25.5 MB (87.60%) - LeetHub

---
+++ 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minSizeSubarray(self, nums: List[int], target: int) -> int:
+        N = len(nums)
+        
+        s = sum(nums)
+        extra_length = N * (target // s)
+        target %= s
+        
+        nums = nums + nums
+        N = len(nums)
+        
+        curr_sum = 0
+        left = 0
+        length = N + 1
+        for right in range(N):
+            curr_sum += nums[right]
+            
+            while curr_sum > target:
+                curr_sum -= nums[left]
+                left += 1
+            
+            if curr_sum == target:
+                length = min(length, right - left + 1)
+        
+        if length == N + 1:
+            return -1
+        
+        return length + extra_length
