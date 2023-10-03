68843fba376c3109a96a2a8e7aa733fbff8050e3
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-10-03 15:14:53 +0300

    Time: 677 ms (7.12%), Space: 18.1 MB (99.28%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,21 +1,14 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        
-        graph = defaultdict(list)
-        for u, v, w, in times:
-            graph[u].append((v, w))
+        # Bellman Ford's Algorithm
         
         distance = {node: float("inf") for node in range(1, n + 1)}
         distance[k] = 0
         
-        heap = [(0, k)]
-        while heap:
-            weight, curr = heapq.heappop(heap)
-            for neighbour, w in graph[curr]:
-                curr_weight = weight + w
-                if curr_weight < distance[neighbour]:
-                    distance[neighbour] = curr_weight
-                    heapq.heappush(heap, (curr_weight, neighbour))
+        for _ in range(n - 1):
+            for u, v, w in times:
+                if distance[u] + w < distance[v]:
+                    distance[v] = distance[u] + w
         
         min_time = 0
         for time in distance.values():
