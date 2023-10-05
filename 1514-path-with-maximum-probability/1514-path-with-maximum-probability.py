23490505e3df77423f5ed0b3ffbe07dcc9f70721
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-10-05 17:23:35 +0300

    Time: 622 ms (37.62%), Space: 28 MB (83.19%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,21 +1,22 @@
 class Solution:
-    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        # dijkstra algorithm
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
         
         graph = defaultdict(list)
         for i in range(len(edges)):
             graph[edges[i][0]].append((edges[i][1], succProb[i]))
             graph[edges[i][1]].append((edges[i][0], succProb[i]))
         
-        prob = {i:-1 for i in range(n)}
-        prob[start] = 1
-        queue = [(-prob[start], start)]
-        while queue:
-            curr_prob, curr = heapq.heappop(queue)
-            curr_prob = -curr_prob
-            for neighbour, cost in graph[curr]:
-                if curr_prob * cost > prob[neighbour]:
-                    prob[neighbour] = curr_prob * cost
-                    heapq.heappush(queue, (-prob[neighbour], neighbour))
+        probablity = [0 for _ in range(n)]
+        probablity[start_node] = 1
+        heap = [(-1, start_node)]        
+        while heap:
+            prob, node = heapq.heappop(heap)
+            prob = prob * -1
+            for neighbour, edge_prob in graph[node]:
+                curr_prob = prob * edge_prob
+                if curr_prob > probablity[neighbour]:
+                    heapq.heappush(heap, (-curr_prob, neighbour))
+                    probablity[neighbour] = curr_prob
         
-        return max(0, prob[end])
+        return probablity[end_node]
+    
