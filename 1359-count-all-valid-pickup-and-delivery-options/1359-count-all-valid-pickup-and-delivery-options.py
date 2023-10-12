72fbdc1639ceec7458db70e0258c8547b6e973ce
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-10-12 18:21:19 +0300

    Time: 49 ms (14.52%), Space: 16.2 MB (67.42%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,8 +1,3 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        result = 1
-        for i in range(2, n + 1):
-            result *= i * (2 * i - 1)
-            result %= (10 ** 9 + 7)
-        
-        return result
+        return (math.factorial(2 * n) // (2 ** n)) % (10 ** 9 + 7)
