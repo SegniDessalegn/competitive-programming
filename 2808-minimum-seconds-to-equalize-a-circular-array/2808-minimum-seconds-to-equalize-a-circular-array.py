0bcd56b710666b601c0bded1cd600519dea15c70
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-11-01 21:06:38 +0300

    Time: 827 ms (13.19%), Space: 50.3 MB (11.90%) - LeetHub

---
+++ 2808-minimum-seconds-to-equalize-a-circular-array/2808-minimum-seconds-to-equalize-a-circular-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumSeconds(self, nums: List[int]) -> int:
+        nums += nums
+        
+        N = len(nums)
+        index = defaultdict(list)
+        for i in range(N):
+            index[nums[i]].append(i)
+        
+        if len(index) == 1:
+            return 0
+        
+        ans = N // 2
+        for num in index:
+            max_range = 0
+            for i in range(1, len(index[num])):
+                max_range = max(max_range, index[num][i] - index[num][i - 1])
+            
+            if max_range > 0:
+                ans = min(ans, max_range // 2)
+        
+        return ans
