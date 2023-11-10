87704baab81b414261fb92f7369bd5ffca568891
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-11-10 09:20:28 +0300

    Time: 978 ms (37.50%), Space: 72 MB (38.31%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -1,44 +1,29 @@
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
-        
+        degree_count = defaultdict(int)
         graph = defaultdict(list)
-        degree = defaultdict(int)
-        for edge in adjacentPairs:
-            graph[edge[0]].append(edge[1])
-            graph[edge[1]].append(edge[0])
-            degree[edge[0]] += 1
-            degree[edge[1]] += 1
+        for u, v in adjacentPairs:
+            graph[u].append(v)
+            graph[v].append(u)
+            degree_count[u] += 1
+            degree_count[v] += 1
         
-        n = len(graph)
         queue = deque()
-        side = 0
-        l, r = 0, n - 1
-        ans = [None for _ in range(n)]
-        for num in graph:
-            if degree[num] == 1:
-                queue.append((num, side))
-                if side == 0:
-                    ans[l] = num
-                    l += 1
-                else:
-                    ans[r] = num
-                    r -= 1
-                side += 1
+        visited = set()
+        for num in degree_count:
+            if degree_count[num] == 1:
+                queue.append(num)
+                visited.add(num)
+                break
         
+        ans = []
         while queue:
-            length = len(queue)
-            for _ in range(length):
-                curr, side = queue.popleft()
-                for neighbour in graph[curr]:
-                    degree[neighbour] -= 1
-                    if degree[neighbour] == 1:
-                        queue.append((neighbour, side))
-                        if side == 0:
-                            ans[l] = neighbour
-                            l += 1
-                        else:
-                            ans[r] = neighbour
-                            r -= 1
+            curr = queue.popleft()
+            ans.append(curr)
+            for neighbour in graph[curr]:
+                if neighbour not in visited:
+                    queue.append(neighbour)
+                    visited.add(neighbour)
         
         return ans
     
