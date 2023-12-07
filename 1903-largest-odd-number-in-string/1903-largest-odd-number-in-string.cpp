40fce410e7163cd013823ef08137030b2a2350da
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-12-07 08:46:15 +0300

    Time: 23 ms (91.89%), Space: 15.2 MB (58.43%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for (int i = num.length() - 1; i >= 0; i--){
+            int n = num[i];
+            if (n % 2){
+                return num.substr(0, i + 1);
+            }
+        }
+        
+        return "";
+    }
+};
