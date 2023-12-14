425cb457bdb6654f766556ef0be7031cb620fc40
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-12-14 19:06:06 +0300

    Time: 528 ms (53.60%), Space: 72.9 MB (35.20%) - LeetHub

---
+++ 2671-frequency-tracker/2671-frequency-tracker.py
@@ -0,0 +1,35 @@
+class FrequencyTracker:
+
+    def __init__(self):
+        self.freq = {}
+        self.freq_freq = defaultdict(int)
+
+    def add(self, number: int) -> None:
+        if number not in self.freq:
+            self.freq[number] = 1
+            self.freq_freq[self.freq[number]] += 1
+        else:
+            self.freq_freq[self.freq[number]] -= 1
+            self.freq_freq[self.freq[number]] = max(0, self.freq_freq[self.freq[number]])
+            self.freq[number] += 1
+            self.freq_freq[self.freq[number]] += 1
+        
+
+    def deleteOne(self, number: int) -> None:
+        if number in self.freq:
+            self.freq_freq[self.freq[number]] -= 1
+            self.freq_freq[self.freq[number]] = max(0, self.freq_freq[self.freq[number]])
+            self.freq[number] -= 1
+            self.freq_freq[self.freq[number]] += 1
+            if self.freq[number] == 0:
+                self.freq.pop(number)
+
+    def hasFrequency(self, frequency: int) -> bool:
+        return self.freq_freq[frequency] > 0
+
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
