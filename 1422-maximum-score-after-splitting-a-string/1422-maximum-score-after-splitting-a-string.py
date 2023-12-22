1951128429a89bdd93e00267827d30870b097bad
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2023-12-22 08:25:18 +0300

    Time: 44 ms (47.92%), Space: 17.3 MB (5.69%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        ans = 0
+        for i in range(1, len(s)):
+            ans = max(ans, s[:i].count("0") + s[i:].count("1"))
+        
+        return ans
+    
