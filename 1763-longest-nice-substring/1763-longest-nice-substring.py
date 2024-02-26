e592a21666e2b2c257a2f394c324f73e5d93013c
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2024-02-26 18:06:30 +0300

    Time: 86 ms (44.01%), Space: 16.6 MB (55.16%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -1,15 +1,29 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
         
-        if len(s) < 2 : return ""
+        def is_valid(state):
+            for i in range(len(state)):
+                if state[i][0] != state[i][1]:
+                    return False
+            return True
         
-        ulSet = set(s)
-        
-        for i,c in enumerate(s):
-            if c.swapcase() not in ulSet:
-                s1 = self.longestNiceSubstring(s[0:i])
-                s2 = self.longestNiceSubstring(s[i+1:])
+        N = len(s)
+        left = 0
+        right = 0
+        max_length = 0
+        for i in range(N):
+            r = ord("z") - ord("A")
+            curr_state = [[False, False] for _ in range(26)]
+            for j in range(i, N):
+                if s[j].islower():
+                    curr_state[ord(s[j].lower()) - 97][0] = True
+                else:
+                    curr_state[ord(s[j].lower()) - 97][1] = True
                 
-                return s2 if len(s2) > len(s1) else s1
-        
-        return s
+                if is_valid(curr_state) and j - i + 1 > max_length:
+                    left = i
+                    right = j
+                    max_length = j - i + 1
+
+        return s[left:right + 1] if right != left else ""
+            
