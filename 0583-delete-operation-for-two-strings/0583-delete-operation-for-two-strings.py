a72952525ddb0a97046540f1cfa93c205986b80b
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2024-05-04 20:59:53 +0300

    Time: 192 ms (26.06%), Space: 20.4 MB (31.46%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -3,17 +3,20 @@ class Solution:
         N1 = len(word1)
         N2 = len(word2)
         
-        @cache
-        def find_ans(i, j):
-            if i == N1 or j == N2:
-                return max(N1 - i, N2 - j)
-            
-            curr_ans = N1 + N2
-            curr_ans = min(curr_ans, 1 + find_ans(i + 1, j), 1 + find_ans(i, j + 1))
-            if word1[i] == word2[j]:
-                curr_ans = min(curr_ans, find_ans(i + 1, j + 1))
-            
-            return curr_ans
+        table = [[float("inf")] * (N2 + 1) for i in range(N1 + 1)]
+        table[0][0] = 0
         
-        return find_ans(0, 0)
+        for i in range(1, N1 + 1):
+            table[i][0] = table[i - 1][0] + 1
+
+        for j in range(1, N2 + 1):
+            table[0][j] = table[0][j - 1] + 1
+        
+        for i in range(1, N1 + 1):
+            for j in range(1, N2 + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    table[i][j] = table[i - 1][j - 1]
+                table[i][j] = min(table[i][j], 1 + table[i][j - 1], 1 + table[i - 1][j])
+        
+        return table[-1][-1]
     
