ff1d5adf4d31c44ce429c05f1f3b0f58f85daabb
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2024-05-12 23:27:14 +0300

    Time: 218 ms (89.45%), Space: 20.1 MB (52.81%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -1,30 +1,33 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
         N = len(grid)
+        dp = [[0] * N for _ in range(N)]
+        dp[-1] = grid[-1][:]
         
-        @cache
-        def dp(i, j):
-            if not(0 <= j < N):
-                return float("inf")
+        def get_mins(arr):
+            idx1 = 0
+            idx2 = 1
+
+            if arr[1] < arr[0]:
+                idx1 = 1
+                idx2 = 0
             
-            if i == N - 1:
-                return grid[i][j]
-            
-            recieved = float("inf")
-            for k in range(N):
-                if k != j:
-                    recieved = min(recieved, dp(i + 1, k))
-            
-            curr = grid[i][j]
-            
-            if recieved != float("inf"):
-                curr += recieved
-            
-            return curr
+            for i in range(2, len(arr)):
+                if arr[i] <= arr[idx1]:
+                    idx2 = idx1
+                    idx1 = i
+                elif arr[i] < arr[idx2]:
+                    idx2 = i
+
+            return (idx1, idx2)
         
-        ans = float("inf")
-        for j in range(N):
-            ans = min(ans, dp(0, j))
-        
-        return ans
+        for i in range(N - 2, -1, -1):
+            idx1, idx2 = get_mins(dp[i + 1])
+            for j in range(N - 1, -1, -1):
+                if j != idx1:
+                    dp[i][j] = grid[i][j] + dp[i + 1][idx1]
+                else:
+                    dp[i][j] = grid[i][j] + dp[i + 1][idx2]
+                
+        return min(dp[0])
     
