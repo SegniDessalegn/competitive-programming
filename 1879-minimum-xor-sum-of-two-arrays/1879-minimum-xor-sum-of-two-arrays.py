b6fe96bdaf6e892330c3708d7b85e80959d4bdbb
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2024-05-17 16:11:07 +0300

    Time: 245 ms (92.98%), Space: 17.1 MB (96.49%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
@@ -2,15 +2,15 @@ class Solution:
     def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
         N = len(nums1)
         
-        dp = [[float("inf")] * (1 << N) for _ in range(N + 1)]
-        dp[0][0] = 0
+        dp = [float("inf")] * (1 << N)
+        dp[0] = 0
         
-        for i in range(N):
-            for mask in range(1 << N):
-                for j in range(N):
-                    if not(mask & (1 << j)):
-                        next_mask = mask | (1 << j)
-                        dp[i + 1][next_mask] = min(dp[i + 1][next_mask], dp[i][mask] + (nums1[i] ^ nums2[j]))
+        for mask in range(1 << N):
+            count = bin(mask).count('1')
+            for j in range(N):
+                if not (mask & (1 << j)):
+                    next_mask = mask | (1 << j)
+                    dp[next_mask] = min(dp[next_mask], dp[mask] + (nums1[count] ^ nums2[j]))
         
-        return dp[N][(1 << N) - 1]
+        return dp[(1 << N) - 1]
     
