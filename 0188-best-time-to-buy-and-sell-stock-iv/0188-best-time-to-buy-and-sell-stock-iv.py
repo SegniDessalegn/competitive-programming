a362fe1e4d0839747b6c77b646ec6c61893be56c
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2024-07-19 07:09:44 +0300

    Time: 252 ms (20.83%), Space: 81.4 MB (9.94%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        
+        @cache
+        def recur(i, can_buy, transactions):
+            if i >= N or transactions >= k:
+                return 0
+            
+            # profit has to be always positive
+            
+            profit = 0
+            if can_buy:
+                profit = max(profit, recur(i + 1, True, transactions), -prices[i] + recur(i + 1, False, transactions))
+            else:
+                profit = max(profit, recur(i + 1, False, transactions), prices[i] + recur(i + 1, True, transactions + 1))
+                
+            return profit
+        
+        N = len(prices)
+        return recur(0, True, 0)
+    
