a6e907288552df685516d995d7b49f543ade162a
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2024-12-11 20:33:22 +0300

    Time: 232 ms (33.48%), Space: 48.5 MB (19.49%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -1,24 +1,35 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.array = [[(0, 0)] for _ in range(length)]
+        self.array = [[[0, 0]] for _ in range(length)]
+        self.can_update = set()
         self.snap_count = 0
 
     def set(self, index: int, val: int) -> None:
-        if self.array[index][-1][0] == self.snap_count:
-            self.array[index][-1] = (self.snap_count, val)
+        if index in self.can_update:
+            self.array[index][1] = val
+            self.can_update.add(index)
         else:
-            self.array[index].append((self.snap_count, val))
+            self.array[index].append([self.snap_count, val])
 
     def snap(self) -> int:
+        self.can_update = set()
         self.snap_count += 1
         return self.snap_count - 1
 
     def get(self, index: int, snap_id: int) -> int:
-        snaps = self.array[index]
-        i = bisect.bisect(snaps, (snap_id + 1,)) - 1
-        return snaps[i][1]
+        left = 0
+        right = len(self.array[index])
         
+        while right - left > 1:
+            mid = (right + left) // 2
+            
+            if self.array[index][mid][0] <= snap_id:
+                left = mid
+            else:
+                right = mid
+        
+        return self.array[index][left][1]
 
 
 # Your SnapshotArray object will be instantiated and called as such:
