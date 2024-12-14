6d1270be5083153d6bcfdb1db86cc1c8c9f5eb74
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2024-12-14 06:51:15 +0300

    Time: 32 ms (84.78%), Space: 17.6 MB (21.11%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
@@ -11,35 +11,22 @@ class Node:
 class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         
-        def traverse(head):
-            if not head:
-                return None
-            
-            if not head.next:
-                if head.child:
-                    tail = traverse(head.child)
-                    head.next = head.child
-                    head.next.prev = head
-                    head.child = None
-                    return tail
-                return head
-            
+        result = head
+        temps = []
+        while head:
             if head.child:
-                tail = traverse(head.child)
-                temp_next = head.next
-                
+                if head.next:
+                    temps.append(head.next)
                 head.next = head.child
                 head.next.prev = head
                 head.child = None
-                
-                tail.next = temp_next
-                tail.next.prev = tail
-                
-                return traverse(temp_next)
-            else:
-                return traverse(head.next)
+            
+            if head.next is None and temps:
+                curr = temps.pop()
+                head.next = curr
+                curr.prev = head
+            
+            head = head.next
         
-        traverse(head)
-        
-        return head
+        return result
     
