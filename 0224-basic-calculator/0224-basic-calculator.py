a3c0eeebb26f3c7c9c9e3745e99f5f28d0ebcc31
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2024-12-27 12:50:35 +0300

    Time: 35 ms (47.33%), Space: 19.6 MB (10.52%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,54 +1,29 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        def get_number(i):
-            num = 0
-            while i < len(s) and s[i] not in "+-=() ":
-                num = num * 10 + int(s[i])
-                i += 1
-            return (num, i)
-        
         s = "(" + s + ")"
         stack = []
-        i = 0
-        while i < len(s):
-            if s[i] == " ":
-                i += 1
+        sign, num = 1, 0
+        
+        for char in s:
+            if char == " ":
                 continue
-            
-            if s[i] == "(":
-                stack.append(s[i])
-            elif s[i] == ")":
+            if char.isdigit():
+                num = num * 10 + int(char)
+            if char in "+-":
+                stack.append(sign * num)
+                sign = 1 if char == "+" else -1
                 num = 0
+            if char == "(":
+                stack.append(sign)
+                stack.append("(")
+                sign, num = 1, 0
+            if char == ")":
+                val = sign * num
                 while stack[-1] != "(":
-                    if stack[-1] == "-":
-                        num *= -1
-                    elif stack[-1] != "+":
-                        num += stack[-1]
-                    stack.pop()
+                    val += stack.pop()
                 stack.pop()
-                if stack and stack[-1] == "-":
-                    stack.pop()
-                    stack.append(num * -1)
-                else:
-                    stack.append(num)
-            elif s[i] in "+-":
-                stack.append(s[i])
-            else:
-                num, i = get_number(i)
-                if stack and stack[-1] == "+" and stack[-2] != "(":
-                    stack.pop()
-                    stack[-1] += num
-                elif stack and stack[-1] == "-" and stack[-2] != "(":
-                    stack.pop()
-                    stack[-1] -= num
-                else:
-                    if stack and stack[-1] == "-":
-                        stack.pop()
-                        stack.append(num * -1)
-                    else:
-                        stack.append(num)
-                continue
-            i += 1
+                stack[-1] *= val
+                sign, num = 1, 0
         
         return stack[0]
     
