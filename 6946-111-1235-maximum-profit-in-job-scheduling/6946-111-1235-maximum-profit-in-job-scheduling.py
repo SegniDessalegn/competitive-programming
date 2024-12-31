97bec0c414c7b3bbf456a86ad0c3315ed75dc92a
Segni <96206349+SegniDessalegn@users.noreply.github.com>
2024-12-31 09:34:02 +0300

    Time: 270 ms, Memory: 30 MB - LeetHub

--- 6946-111-1235-maximum-profit-in-job-scheduling/6946-111-1235-maximum-profit-in-job-scheduling.py
+++ 6946-111-1235-maximum-profit-in-job-scheduling/6946-111-1235-maximum-profit-in-job-scheduling.py
@@ -1,33 +1,34 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         
-        def get_next(i):
-            left = i
-            right = N
+        def get_prev(i):
+            left = -1
+            right = i
             
             while right - left > 1:
                 mid = (left + right) // 2
-                if jobs[mid][0] >= jobs[i][1]:
-                    right = mid
-                else:
+                if jobs[mid][1] <= jobs[i][0]:
                     left = mid
+                else:
+                    right = mid
             
-            return right
-        
-        @cache
-        def get_ans(i):
-            if i >= N:
-                return 0
-            
-            return max(jobs[i][2] + get_ans(get_next(i)), get_ans(i + 1))
-        
+            return left
         
         N = len(startTime)
         jobs = []
         for i in range(N):
             jobs.append((startTime[i], endTime[i], profit[i]))
         
-        jobs.sort()
+        jobs.sort(key = lambda job: job[1])
         
-        return get_ans(0)
+        dp = [0] * (N + 1)
+        for i in range(1, N + 1):
+            # take
+            dp[i] = jobs[i - 1][2] + dp[get_prev(i - 1) + 1]
+            
+            # no take
+            dp[i] = max(dp[i], dp[i - 1])
+        
+        
+        return dp[-1]
     
